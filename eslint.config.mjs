87dee01a3cb3967192d87c714bc4def6fcc9c18a
irajatsharma10 <rajat1023982@gmail.com>
2025-01-27 20:33:15 +0530

    actionTypes handling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off', // Disable the rule
+      '@typescript-eslint/no-explicit-any': 'off',
+       // Disable the rule
+       '@typescript-eslint/no-unused-vars' : 'off',
     },
   },
 ];
